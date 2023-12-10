f2ba2e35f04f0036634d509737dacc8767b29176
Aki-Ito <60352606+Aki-Ito@users.noreply.github.com>
2023-12-10 10:11:17 +0900

    complete

---
+++ CollectionViewTile/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewTile
+//
+//  Created by 伊藤明孝 on 2023/12/10.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
